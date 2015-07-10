8e450817a04b54b8b64881fd05772086c99de5c9
MVesuviusC <frodo5343@hotmail.com>
2015-07-10 10:42:25 -0400

    Updated documentation and changed name of the table.primerTree function to seq_lengths in order to avoid issues with parameters showing up properly and avoid mixing up the base::table functionality.

--- R/filterResults.R
+++ R/filterResults.R
@@ -6,26 +6,19 @@
 #' short or too long. The alignment and tree will be recalculated after removing
 #' unwanted reads.
 #' @param x a primerTree object
+#' @param min_length the minimum sequence length to keep
+#' @param max_length the maximum sequence length to keep
 #' @return a primerTree object
 #' @examples
 #' 
 #' # filter out sequences longer or shorter than desired:
 #' mammals_16S_filtered <- filter_seqs(mammals_16S, min_length=131, max_length=156)
-#' 
 #' @export
-
 filter_seqs = function(x, ...) UseMethod("filter_seqs")
 
-#' Filter out sequences retrieved by search_primer_pair() that are either too
-#' short or too long. The alignment and tree will be recalculated after removing
-#' unwanted reads.
-#' @param primertree_obj a primerTree object
-#' @param min_length the minimum sequence length to keep
-#' @param max_length the maximum sequence length to keep
-#' @return a primerTree object
+#' Method for primerTree objects
+#' @inheritParams filter_seqs
 #' @export
- 
-
 filter_seqs.primerTree = function(primertree_obj, min_length = 0, max_length = Inf ) {
   #if(is.null(primerTreeObj$sequence)) {
   #  print "No sequences in primerTree object"
@@ -43,38 +36,35 @@ filter_seqs.primerTree = function(primertree_obj, min_length = 0, max_length = I
       
   # realign filtered sequences
   primertree_obj$alignment <- clustalo(primertree_obj$sequence)
+  
   # remake tree
   primertree_obj$tree <- tree_from_alignment(primertree_obj$alignment)
   primertree_obj
 }
 
-#' Get sequence lengths from a primerTree object
-#' Also returns a summary of the sequence lengths 
-#' @param primertree_obj a primerTree object
+#' Get a summary of sequence lengths from a primerTree object
+#' @param x a primerTree object. 
 #' @param summarize a logical indicating if a summary should be displayed
-#' @export
- 
-table <- function(...) UseMethod("table")
-table.default <- base::table
-
-#' Get sequence lengths from a primerTree object
-#' Also returns a summary of the sequence lengths 
-#' @param primertree_obj a primerTree object
-#' @param summarize a logical indicating if a summary should be displayed
-#' @return sequence lengths
+#' @return a table of sequence length frequencies
 #' @examples 
 #' 
 #' # Show the counts for each length
-#' table(mammals_16S)
+#' seq_lengths(mammals_16S)
 #' 
 #' # Plot the distribution of lengths
-#' seqLengths <- table(mammals_16S)
-#' barplot(seqLengths)
-#' 
+#' seqLengths <- seq_lengths(mammals_16S)
+#' barplot(seqLengths, 
+#'  main = "Frequency of sequence lengths for 16S mammal primers", 
+#'  xlab="Amplicon length (in bp)", 
+#'  ylab=("Frequency"))
 #' @export
+seq_lengths <- function(x,summarize = TRUE) UseMethod("seq_lengths")
 
-table.primerTree <- function(primertree_obj, summarize = TRUE) {
-  lengths <- unlist(lapply(primertree_obj$sequence, length))
+#' Method for primerTree objects
+#' @inheritParams seq_lengths
+#' @export
+seq_lengths.primerTree <- function(x, summarize = TRUE) {
+  lengths <- unlist(lapply(x$sequence, length))
   message("sequence length distribution: ")
   table(as.factor(lengths))
 }
